38ebfefedb4fb9b828c9a1f9c7a754b48b63a5b8
Nikolaos Bezirgiannis <bezirg@users.noreply.github.com>
2022-02-02 14:37:26 +0100

    SCP-3409: EXPERIMENT Change flat index to encode through word64

--- nix/pkgs/haskell/materialized-linux/.plan.nix/plutus-core.nix
+++ nix/pkgs/haskell/materialized-linux/.plan.nix/plutus-core.nix
@@ -557,6 +557,7 @@
             "DeBruijn/Scope"
             "DeBruijn/Spec"
             "DeBruijn/UnDeBruijnify"
+            "DeBruijn/FlatNatWord"
             ];
           hsSourceDirs = [ "untyped-plutus-core/test" ];
           mainPath = [ "Spec.hs" ];
